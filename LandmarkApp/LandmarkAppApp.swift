47bfeee56f28223269d436b6aa5c8b0d505243dc
C. Atahan Özbek <atahanozzbek@gmail.com>
2024-12-25 17:11:43 +0300

    Initial Commit

---
+++ LandmarkApp/LandmarkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkAppApp.swift
+//  LandmarkApp
+//
+//  Created by cenk atahan özbek on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
